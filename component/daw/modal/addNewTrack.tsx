8d111e017841d17f66bbb2fe1c42580505376141
Yoonsoo Kim <laptise@live.jp>
2021-12-04 23:34:03 +0900

    トラック追加

--- component/daw/modal/addNewTrack.tsx
+++ component/daw/modal/addNewTrack.tsx
@@ -1,10 +1,30 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DawContext } from "..";
+import { Track } from "../../../firebase/model";
 import Modal from "../../modal";
 
 interface Props {}
 const AddNewTrackModal: React.FC<Props> = () => {
   const { addNewModalViewState } = useContext(DawContext);
-  return <Modal title={"新規トラック追加"} id="AddNewTrackModal" viewState={addNewModalViewState}></Modal>;
+  const [trackNm, setTrackNm] = useState("");
+  const [view, setView] = addNewModalViewState;
+  const submit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setView(false);
+    const newTrack = (() => {
+      const item = new Track();
+      item.name = trackNm;
+      return item;
+    })();
+  };
+  return (
+    <Modal title={"新規トラック追加"} id="AddNewTrackModal" viewState={addNewModalViewState}>
+      <form onSubmit={submit}>
+        <label>トラック名</label>
+        <input value={trackNm} onInput={(e) => setTrackNm(e.currentTarget.value)} />
+        <button type="submit">OK</button>
+      </form>
+    </Modal>
+  );
 };
 export default AddNewTrackModal;
